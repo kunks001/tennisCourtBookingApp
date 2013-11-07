45675e688bf46766f01cc3eeb548ecb28e50d944
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-11-07 12:41:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TennisCourtBookingApp::Application.config.secret_key_base = '301696049220131075e0bc4795ccccf71f8e70ceed90938ae6a03b0af69dcf35b7880802b7b1b661a6f37cbfa58810c391ca4868182bd2a8f929f75cc5fe7e6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TennisCourtBookingApp::Application.config.session_store :cookie_store, key: '_tennisCourtBookingApp_session'
